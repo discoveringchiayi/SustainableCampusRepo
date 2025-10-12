b898889ab4b3bd7e7b8a61c31b96318d09e1cdbb
Axolotl5517 <discoveringchiayi@gmail.com>
2025-10-13 01:22:12 +0800

    add virtual tour

---
+++ data.js
@@ -0,0 +1,169 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.4564048086789114,
+        "pitch": 0.05233193267193137,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32590193426032066,
+          "pitch": 0.07819689421576648,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.691243591147744,
+          "pitch": 0.05774989479375847,
+          "rotation": 4.71238898038469,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.6877292364820349,
+        "pitch": 0.02735915565016711,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6904926919890233,
+          "pitch": 0.02440176318892462,
+          "rotation": 17.27875959474387,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.765365336914005,
+        "pitch": 0.21071664426319003,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.659815493577316,
+          "pitch": 0.027457915875356065,
+          "rotation": 13.351768777756625,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6276637655008992,
+          "pitch": 0.0456348191721716,
+          "rotation": 4.71238898038469,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
